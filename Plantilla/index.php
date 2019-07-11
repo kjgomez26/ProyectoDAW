be1fec72273e0bb109f1f893abb5edc667c320a7
Luiggi alarcon <luiggis.ao@hotmail.com>
2019-07-10 23:30:55 -0500

    a

--- Plantilla/index.php
+++ Plantilla/index.php
@@ -1,1 +1,8 @@
-<?php header( 'Location: ./index.html' ) ;  ?> 
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+<?php header( 'Location: ./index.html' ) ;  ?>
+  </body>
+</html> 

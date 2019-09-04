bad24b9d06f2c401fa6fafb815ab5f88b4851b66
Tonny1494 <agespino@espol.edu.ec>
2019-09-04 00:21:43 -0500

    cambio en newscomponent

--- frontend/client/src/components/NewsComponent.vue
+++ frontend/client/src/components/NewsComponent.vue
@@ -3,176 +3,165 @@
    <div class="news">
      <div class="row">
        <div class="col-lg-8">
-         <div class="news_item">
+         <div class="news_items">
            <div class="news_item">
              <div class="new_image_container">
-               <div class="new_image">
-                 <img src="image/news_1.jpg" alt="">
-               </div>
+              <div class="new_image">
+                <img src="image/news_1.jpg" alt="">
+              </div>
              </div>
-           <div class="news_body">
-               <div class="news_title">
-                 <a href="#">Cursos Libres Individuales>
-                 </a>
+             <div class="news_body">
+              <div class="news_title"><a href="#">Cursos Libres Individuales></a>
+              </div>
+              <div class="new_info">
+              </div>
+              <div class="new_text">
+                <p>Abierto para grupos interesados en la música,este programa se adapta a persona de todas las edades,mediante un esquema flexible</p>
+                <strong>Horarios</strong>
+                <p>Mañana 9h00 a 14h00
+                Tardes 15h00 a 20h00</p>
+                <strong>Sede</strong> 
+                <p>Conservatorio de Música "Nicolo Paganini"
+                Cdla Garzota 1.Av.Hermano Miguel Mz.21 villa 20
+                Guayaquil-Ecuador</p>
+                <strong>Requisito</strong>
+                <p>Copia de cedula de identidad y foto tamaño carnet actualizado
+                </p>
+              </div>
+              <div class="button new_button"><a href="#">Inscribite ahora></a>
+              </div>
+             </div> 
+           </div> 
+           <div class="news_item">
+              <div class="new_image_container">
+                <div class="news_image">
+                 <img src="images/news_2.jpg" alt="">
+                </div>
+              </div>
+             <div class="new_body">
+               <div class="new_title"><a href="#">Cursos Libres Grupales</a>
                </div>
                <div class="new_info">
                </div>
                <div class="new_text">
-                 <p>Abierto para grupos interesados en la música,este programa se adapta a persona de todas las edades,mediante un esquema flexible</p>
+                 <p>Abierto para todos aquellos interesados en la musica,este programa se adapta a todas las edades,mediante un esquema flexible.</p>
                  <strong>Horarios</strong>
-                 <p>Mañana 9h00 a 14h00
-                 Tardes 15h00 a 20h00</p>
-                 <strong>Sede</strong> 
-                 <p>Conservatorio de Música "Nicolo Paganini"
-                 Cdla Garzota 1.Av.Hermano Miguel Mz.21 villa 20
+                 <p>Mañana: 9h00 a 14h00
+                 Tardes:15h00 a 20h00</p>
+                 <strong>Sede</strong>
+                 <p>conservatorio de Musica "Niccoló Paganini"
+                 Cdla.Garzota 1.Av. Hermano Miguel Mz 21 villa 20
                  Guayaquil-Ecuador</p>
                  <strong>Requisito</strong>
-                 <p>Copia de cedula de identidad y foto tamaño carnet actualizado
-                 </p>
-               <div class="button new_button">
-                 <a href="#">Inscribite ahora>
-                 </a>
+                 <p>Copia de cedula de identidad y foto tamaño carnet actualizada</p> 
                </div>
-           </div> 
-         </div>
-         <div class="new_itens">
-           <div class="new_image_container">
-             <div class="news_image">
-               <img src="images/news_2.jpg" alt="">
+               <div class="button news_button"><a href="#">Inscribete ahora></a>
+               </div>
              </div>
            </div>
-           <div class="new_body">
-             <div class="new_title">
-               <a href="#">Cursos Libres Grupales
-               </a>
-             </div>
-             <div class="new_info">
-             </div>
-             <div class="new_text">
-               <p>Abierto para todos aquellos interesados en la musica,este programa se adapta a todas las edades,mediante un esquema flexible.</p>
-               <strong>Horarios</strong>
-               <p>Mañana: 9h00 a 14h00
-               Tardes:15h00 a 20h00</p>
-               <strong>Sede</strong>
-               <p>conservatorio de Musica "Niccoló Paganini"
-               Cdla.Garzota 1.Av. Hermano Miguel Mz 21 villa 20
-               Guayaquil-Ecuador</p>
-               <strong>Requisito</strong>
-               <p>Copia de cedula de identidad y foto tamaño carnet actualizada</p> 
-             </div>
-             <div class="button news_button">
-               <a href="#">Inscribete ahora>
-               </a>
-             </div>
-           </div>
-         </div>
-         <div class="new items">
-           <div class="new_image_container">
-             <div class="news_image">
-               <img src="images/news_2.jpg" alt="">
-             </div>
-             <div class="new body">
-               <a href="#">Curso Formales
-               </a>
-             </div>
-             <div class="new info">
-             </div>
-             <div class="new_text">
-               <p>El programa formal prepara a los alumnos para la obtencion del titulo de "Bachiller en Música"(Acuerdo minesterial 3358).</p>
-               <strong>Niveles</strong>
-               <p>Parvulario,Inicial y Técnico</p>
-               <strong>Sede</strong>
-               <p>Conservatorio de Música "Niccolo Paganini"
-               Cdla.Garzota.1.Av Hermano Miguel Mz. 21 villa 20
-               Guayaquil-Ecuador</p>
-             </div>
-             <div class="button news_button">
-               <a href="#">Inscribete ahora>
-               </a>
-             </div>
+           <div class="new item">
+              <div class="new_image_container">
+              <div class="news_image">
+                <img src="images/news_2.jpg" alt="">
+                </div>
+              </div>
+              <div class="new body">
+               <div class="news_title"><a href="#">Programa Formal</a>
+               </div>
+               <div class="new info">
+               </div>
+               <div class="new_text">
+                 <p>El programa formal prepara a los alumnos para la obtencion del titulo de "Bachiller en Música"(Acuerdo minesterial 3358).</p>
+                 <strong>Niveles</strong>
+                 <p>Parvulario,Inicial y Técnico</p>
+                 <strong>Sede</strong>
+                 <p>Conservatorio de Música "Niccolo Paganini"
+                 Cdla.Garzota.1.Av Hermano Miguel Mz. 21 villa 20
+                 Guayaquil-Ecuador</p>
+                 <strong>Requisito</strong>
+                 <p>Copia de cedula de identidad y foto tamaño carnet actualizada</p> 
+               </div>
+               <div class="button news_button">
+                 <a href="#">Inscribete ahora></a>
+               </div>
+              </div>
            </div>
          </div>
        </div>
        <div class="col-lg-4">
-         <div class="sidebar">
-           <div class="tickets">
-             <div class="Background_image" style="background-image:url(image/tickets.jpg)">
-             </div>
-             <div class="tickets_iner text-center">
-               <div class="tickets_title">Inscripciones Abiertas
-               </div>
-               <div class="tickets_text">No te pierdas nuestro nuevos cursos para este año lectivo 2019-2020.
-               </div>
-               <div class="tickets_next">
-                 <a href="http://teatrosanchezaguilar.org/solistas-ensambles-niccolo-paganini/?fbclid=IwAR0iVQ8yIOr8pQ-GDLG4qaqoR0i3NJOEmUfH-1aYy6becOQ87z9PFRh2REI">Conoce nuestros proximos eventos.
-                 </a>
-             </div>
-         </div>
-         <div class="sidebar_categories">
-           <div class="sidebar_categories_title">Categorias
-             </div>
-               <ul class="categories_list">
-                 <li><a href="#">Voz</a></li>
-                 <li><a href="#">Cuerda</a></li>
-                 <li><a href="#">Percusión</a></li>
-                 <li><a href="#">Viento</a></li>
-                 <li><a href="#">Ensambles</a></li>
-               </ul>
-           </div> 
-         </div>
-         <div class="latest_post">
-           <div class="latest_post_title">Chequea nuestros ultimos post
-           </div>
-           <div class="latest_container">
-             <div class="latest d-flex flex-row align-items-start justify-content-start">
-               <div>
-                 <div class="latest_image"><img src="images/latest_1.jpg" alt="">
-                 </div>
-               </div>
-               <div class="latest_content">
-                 <div class="latest_title">
-                   <a href="https://www.instagram.com/p/BzqGUJHgw1f/">III Festival Internacional de orquestas y ensambles
-                   </a>
-                 </div>
-                 <div class="latest_date">Julio 08, 2019
-                 </div>
-               </div>
-             <div class="latest d-flex flex-row align-items-start justify-content-start">
-               <div>
-                 <div class="latest_image"><img src="images/latest_3.jpg" alt="">
-                 </div>
-               </div>
-               <div class="latest_content">
-                 <div class="latest_title">
-                   <a href="https://www.instagram.com/p/Bzi8yxcAs3P/">3er Concierto de Temporada en Homenaje a Guayaquil.
-                   </a>
-                 </div>
-                 <div class="latest_date">Julio 05, 2019
-                 </div>
-               </div>
-             </div>
-             <div class="latest d-flex flex-row align-items-start justify-content-start">
-               <div>
-                 <div class="latest_image"><img src="images/latest_2.jpg" alt="">
-                 </div>
-               </div>
-               <div class="latest_content">
-                 <div class="latest_title">
-                   <a href="https://www.instagram.com/p/Bzi8yxcAs3P/">Archivo del Festival Internacional De Música Guaranda - Masterclass en Guayaquil
-                   </a>
-                 </div>
-                 <div class="latest_date">Julio 02, 2019
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
+        <div class="sidebar">
+          <div class="tickets">
+            <div class="Background_image" style="background-image:url(image/tickets.jpg)">
+            </div>
+            <div class="tickets_iner text-center">
+              <div class="tickets_title">Inscripciones Abiertas
+              </div>
+              <div class="tickets_text">No te pierdas nuestro nuevos cursos para este año lectivo 2019-2020.
+              </div>
+              <div class="tickets_next">
+                <a href="http://teatrosanchezaguilar.org/solistas-ensambles-niccolo-paganini/?fbclid=IwAR0iVQ8yIOr8pQ-GDLG4qaqoR0i3NJOEmUfH-1aYy6becOQ87z9PFRh2REI">Conoce nuestros proximos eventos.</a>
+            </div>
+          </div>   
+          <div class="sidebar_categories">
+            <div class="sidebar_categories_title">Categorias
+            </div>
+              <ul class="categories_list">
+                <li><a href="#">Voz</a></li>
+                <li><a href="#">Cuerda</a></li>
+                <li><a href="#">Percusión</a></li>
+                <li><a href="#">Viento</a></li>
+                <li><a href="#">Ensambles</a></li>
+              </ul>
+          </div> 
+          <div class="latest_post">
+            <div class="latest_post_title">Chequea nuestros ultimos post</div>
+            <div class="latest_container">
+              <div class="latest d-flex flex-row align-items-start justify-content-start">
+                <div>
+                  <div class="latest_image"><img src="images/latest_1.jpg" alt=""></div>
+                </div>
+                <div class="latest_content">
+                  <div class="latest_title">
+                    <a href="https://www.instagram.com/p/BzqGUJHgw1f/">III Festival Internacional de orquestas y ensambles</a>
+                  </div>
+                  <div class="latest_date">Julio 08, 2019
+                  </div>
+                </div>
+              </div>
+              <div class="latest d-flex flex-row align-items-start justify-content-start">
+                <div>
+                  <div class="latest_image"><img src="images/latest_3.jpg" alt="">
+                  </div>
+                </div>
+                <div class="latest_content">
+                  <div class="latest_title">
+                    <a href="https://www.instagram.com/p/Bzi8yxcAs3P/">3er Concierto de Temporada en Homenaje a Guayaquil.</a>
+                  </div>
+                  <div class="latest_date">Julio 05, 2019
+                  </div>
+                </div>
+              </div>
+              <div class="latest d-flex flex-row align-items-start justify-content-start">
+                <div>
+                  <div class="latest_image"><img src="images/latest_2.jpg" alt="">
+                  </div>
+                </div>
+                <div class="latest_content">
+                  <div class="latest_title">
+                    <a href="https://www.instagram.com/p/Bzi8yxcAs3P/">Archivo del Festival Internacional De Música Guaranda-Masterclass en Guayaquil</a>
+                  </div>
+                  <div class="latest_date">Julio 02, 2019
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
        </div>
      </div>
-   </div>
-   <div class="cta">
+    </div>
+    <div class="cta">
      <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/foo.jpg"
-        data-speed="0.8">
+      data-speed="0.8">
      </div>
      <div class="container">
        <div class="row">
@@ -184,6 +173,6 @@
          </div>
        </div>
      </div>
-   </div>
- </div>
+    </div>
+  </div>
 </template>

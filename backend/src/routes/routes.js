dbbba8558df128d07dc27115e8a6ec8fb5eb2110
thsam <planetxz@hotmail.com>
2019-09-04 23:25:18 -0500

    email

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,11 @@
 // Load the MySQL pool connection
 const pool = require('../data/config');
+//thai inicio
+nodeMailer = require('nodemailer');
+/*app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());*/
 
+//thai fin
 
 const router = app => {
     // Display welcome message on the root
@@ -151,6 +156,54 @@ const router = app => {
             response.send('Evento eliminado.');
         });
     });
+    /*TODO: Thai*/
+    app.post('/send-email', function (req, res) {
+        let transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'youemail@gmail.com',//process.env.EMAIL || 'abc@gmail.com', // TODO: your gmail account
+                pass: 'yourpass'//process.env.PASSWORD || '1234' // TODO: your gmail password
+            }
+        });
+        
+        // Step 2
+        let mailOptions = {
+            from: 'abc@gmail.com', // TODO: email sender
+            to: 'thaiilizina@gmail.com', // TODO: email receiver
+            subject: 'Nodemailer - Test',
+            text: 'Wooohooo it works!!'
+        };
+        
+        // Step 3
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
+                return log('Error occurs');
+            }
+            return log('Email sent!!!');
+        })
+        /*let transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user:'thaiilizina@gmail.com',
+                clientId: '1050554924440-5t7gq43929a0trql3sgpapauo0n8pjnh.apps.googleusercontent.com',
+                clientSecret: 'BPFOYeqbLur9TfNBTsfKUaiQ',
+                refreshToken: '1/hWJs5E0cxTJTVVeTAxJABxiJKZEzO4KkegU8xfqnV7kYOUR_REFRESH_TOKEN'
+            },
+            
+            tls:{
+                rejectUnauthorized:false
+            }
+        });*/
+        
+       
+       
+ 
+}); //post
+
+     
 }
 
 module.exports = router;

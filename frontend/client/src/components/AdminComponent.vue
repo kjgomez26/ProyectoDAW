5334e6993a7ac3bc70fd1c2ffc3a2efdcf4b59bd
Tonny1494 <agespino@espol.edu.ec>
2019-09-05 14:11:03 -0500

    creacion Admin component

---
+++ frontend/client/src/components/AdminComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <div>
+   <table border="1">
+     <caption>Suscriptores</caption>
+     <tr>
+       <th>Correo</th>
+       <th>Nombre</th>
+       <th>Ciudad</th>
+       <th>Usuario</th>
+     </tr>
+   </table>
+  </div> 
+  <div>
+    <table border="1">
+     <caption>Evento</caption>
+      <tr>
+       <th>IdEvento</th>
+       <th>Titulo</th>
+       <th>Descripcion</th>
+       <th>Fecha</th>
+       <th>Lugar</th>
+       <th>Organizador</th>
+      </tr>
+    </table>
+   </div> 
+  </div>
+</template>
